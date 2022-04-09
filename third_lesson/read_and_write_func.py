c33b86a7dca0a3866603586738ab66e9e9a8f450
AnnaS <anya.keyn@gmail.com>
2022-04-09 18:42:21 +0300

    print removed

--- third_lesson/read_and_write_func.py
+++ third_lesson/read_and_write_func.py
@@ -7,7 +7,6 @@ with open("../third_lesson/files/users.json", "r") as f:
     users = json.loads(f.read())
 
     for user in users:
-        index = user['index']
         users_list.append({'name': user['name'], 'gender': user['gender'], 'address': user['address'],
                            'age': user['age'], 'books': []})
 
@@ -22,7 +21,6 @@ with open('../third_lesson/files/books.csv', 'r') as f:
 iter_books = (book for book in books_list)
 for u in cycle(users_list):
     try:
-        print(u['name'], u['books'])
         u['books'].append(next(iter_books))
     except StopIteration:
         break
